55f584e20d851104617bae3513e03ae57ece57d4
Arthur Fudali <arthurfudali5@gmail.com>
2024-11-13 14:43:26 -0300

    feat: funcao merge sort

--- main.py
+++ main.py
@@ -157,6 +157,39 @@ def display_heatmap(heatmap):
     screen.blit(heatmap_surface, (0, 0))  
     pygame.display.flip()  
 
+# Função para o algoritmo de Merge Sort
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        # Ordena e combina as sub-listas
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+
 # Inicia a câmera 
 cap = cv2.VideoCapture(0)  
 running = True  
